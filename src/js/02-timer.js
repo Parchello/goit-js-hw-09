71e42389cfe9f352b11ffb2f76d8a4283664b455
Parchello <parchellos@gmail.com>
2023-08-20 13:38:42 +0300

    3 task

---
+++ src/js/02-timer.js
@@ -0,0 +1,46 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const onTimer = document.querySelector('[data-start]');
+onTimer.addEventListener('click', timerStart);
+
+const flatpickrInstance = flatpickr('#datetime-picker', options);
+
+function timerStart() {
+  currentDate = new Date();
+
+  const selectedDate = flatpickrInstance.selectedDates[0];
+  const diferense = selectedDate - currentDate;
+  console.log(diferense);
+  console.log('start date: ', selectedDate);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+console.log(convertMs());
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+addEventListener('submit', listPromises);
+
+function listPromises(evt) {
+  evt.preventDefault();
+  const firstDelay = parseInt(
+    document.querySelector('input[name="delay"]').value
+  );
+  const step = parseInt(document.querySelector('input[name="step"]').value);
+  const amount = parseInt(document.querySelector('input[name="amount"]').value);
+
+  const arr = [];
+  for (let i = 0; i < amount; i++) {
+    const delay = firstDelay + i * step;
+    arr.push(createPromise(i, delay));
   }
+  function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
+  arr.forEach(promise => {
+    promise
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  });
 }
