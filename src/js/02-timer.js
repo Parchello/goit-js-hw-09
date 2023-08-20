fce85da84236abde05b37543e23863a98d08c971
Parchello <parchellos@gmail.com>
2023-08-20 19:29:56 +0300

    finished hw

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const options = {
   enableTime: true,
@@ -13,34 +14,48 @@ const options = {
 
 const onTimer = document.querySelector('[data-start]');
 onTimer.addEventListener('click', timerStart);
+const numbers = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
 
 const flatpickrInstance = flatpickr('#datetime-picker', options);
 
 function timerStart() {
-  currentDate = new Date();
+  onTimer.disabled = true;
+  const timerId = setInterval(() => {
+    currentDate = new Date();
+    const selectedDate = flatpickrInstance.selectedDates[0];
+    const diferense = selectedDate - currentDate;
+    if (selectedDate < currentDate) {
+      Notiflix.Notify.failure('You  must choose future date!!!');
+      clearInterval(timerId);
+      return;
+    }
+    convertMs(diferense);
+  }, 1000);
 
-  const selectedDate = flatpickrInstance.selectedDates[0];
-  const diferense = selectedDate - currentDate;
-  console.log(diferense);
-  console.log('start date: ', selectedDate);
+  function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const days = addLeadingZero(Math.floor(ms / day));
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    const seconds = addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
+
+    numbers.days.textContent = days;
+    numbers.hours.textContent = hours;
+    numbers.minutes.textContent = minutes;
+    numbers.seconds.textContent = seconds;
+    return { days, hours, minutes, seconds };
+  }
+  function addLeadingZero(value) {
+    return value.toString().padStart(2, '0');
+  }
 }
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-console.log(convertMs());
